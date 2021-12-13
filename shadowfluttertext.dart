bd8f8e89961e35a33ec854b6ca16709262fb6633
Ruri <74637030+Mabruri99@users.noreply.github.com>
2021-12-12 22:04:22 -0500

    Create shadowfluttertext.dart

---
+++ shadowfluttertext.dart
@@ -0,0 +1,8 @@
+const Text(
+          'Geniuz App',
+          style: TextStyle(shadows: [
+            Shadow(color: Colors.black, blurRadius: 4, offset: Offset(2, 2)),
+            Shadow(color: Colors.black, blurRadius: 4, offset: Offset(3, 3)),
+            Shadow(color: Colors.black, blurRadius: 4, offset: Offset(4, 4)),
+          ], fontWeight: FontWeight.bold, fontFamily: 'QuickSans'),
+        ),
