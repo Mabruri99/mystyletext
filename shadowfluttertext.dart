a5e3db41aed7739328da1dfdb38d26271b785922
Ruri <74637030+Mabruri99@users.noreply.github.com>
2021-12-12 22:17:52 -0500

    Update shadowfluttertext.dart

--- shadowfluttertext.dart
+++ shadowfluttertext.dart
@@ -1,8 +1,20 @@
 const Text(
-          'Geniuz App',
-          style: TextStyle(shadows: [
-            Shadow(color: Colors.black, blurRadius: 4, offset: Offset(2, 2)),
-            Shadow(color: Colors.black, blurRadius: 4, offset: Offset(3, 3)),
-            Shadow(color: Colors.black, blurRadius: 4, offset: Offset(4, 4)),
-          ], fontWeight: FontWeight.bold, fontFamily: 'QuickSans'),
+          'Ruri Geniuz',
+          style: TextStyle(
+            shadows: [
+              Shadow(
+                  color: Colors.black45, blurRadius: 7, offset: Offset(1, 3)),
+              Shadow(
+                  color: Colors.black45, blurRadius: 7, offset: Offset(2, 4)),
+              Shadow(
+                  color: Colors.black45, blurRadius: 7, offset: Offset(3, 5)),
+              Shadow(
+                  color: Colors.black45, blurRadius: 7, offset: Offset(4, 6)),
+              Shadow(
+                  color: Colors.black45, blurRadius: 7, offset: Offset(5, 7)),
+            ],
+            fontWeight: FontWeight.bold,
+            fontFamily: 'QuickSans',
+            fontSize: 30,
+          ),
         ),
